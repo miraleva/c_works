c592d4e26df86640d708988ce53eb0b9c4ab6bd9
almira mirza <miramirza74@gmail.com>
2025-03-08 16:46:01 +0300

    first commit

---
+++ helloWorld.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void selam(){
+    printf("hi\n");
+}
+
+int main(){
+    int degisken= 1;
+    if(degisken==1){
+        printf("hallo\n");
+        
+    }else{
+        printf("hello");
+    }
+
+    for(int i=0; i<17; i++){
+        printf("%x\n",i);
+    }
+
+    int i=15*16;
+    printf("%x\n",i);
+
+   
+    printf("%d\n",i);
+
+
+    selam();
+
+    printf("Hallo Welt.\n");
+    return 0;
+
+    
+    
+    
+}
+
+
---
+++ work.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("hatirliyorum hell yea\n"); //bide ing karakter kullan yazarken!! 
+    return 0; //bunda da not böyle yazılıyo 
+}

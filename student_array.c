20073393a849582679d3f7031bdf88c70e89b002
almira mirza <miramirza74@gmail.com>
2025-03-14 02:24:29 +0300

    bittiy

--- student_array.c
+++ student_array.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    char *students[]={"Fatma", "Zeynep","Harun","Can","Yeter"};
-    for(int i=0; i<5; i++){
-        printf(students[i]);
+    char *students[]={"Fatma ","Harun " ,"Yeter "};
+    int math[]={50, 60, 70};
+    int history[]={40, 60, 20};
+    int science[]={20, 59, 30}; 
+    
+    for(int i=0; i<3; i++){
+        printf("Names: %s Math: %d History:%d Science:%d\n", students[i],math[i],history[i],science[i]);
     }
 
 }
